8c3a650f14b6e4eb42269b6dd390b82b69011f8d
QuenMBar <Qmb2@students.calvin.edu>
2018-10-18 23:26:15 -0400

    Homework 2 - InProgress

--- Homework2/app/src/main/java/qmb2/cs262/calvin/edu/homework2/MainActivity.java
+++ Homework2/app/src/main/java/qmb2/cs262/calvin/edu/homework2/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -35,11 +36,34 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         if(getSupportLoaderManager().getLoader(0)!=null){
             getSupportLoaderManager().initLoader(0,null,this);
         }
+
+        String queryString = "base";
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length()!=0) {
+            mTitleText.setText(R.string.loading_text);
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, this);
+        }
+
+        else {
+            if (queryString.length() == 0) {
+                mTitleText.setText("Please enter a search term");
+            } else {
+                mTitleText.setText("Please check your network connection and try again.");
+            }
+        }
     }
 
     public void searchBooks(View view) {
         String queryString = mBookInput.getText().toString();
-
+        if (queryString.length() == 0) {
+            queryString = "base";
+        }
         InputMethodManager inputManager = (InputMethodManager)
                 getSystemService(Context.INPUT_METHOD_SERVICE);
         inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
@@ -121,10 +145,32 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 mTitleText.setText("2");
             }
 
-        } catch (Exception e){
-            // If onPostExecute does not receive a proper JSON string, update the UI to show failed results.
-            mTitleText.setText("1");
-            e.printStackTrace();
+        } catch(Exception e) {
+            try {
+                JSONObject jsonObject = new JSONObject(data);
+                String title = null;
+                String id = jsonObject.getString("id");
+                String eMail = jsonObject.getString("emailAddress");
+                String name = null;
+                try {
+                    name = jsonObject.getString("name");
+                } catch (Exception q) {
+                    name = "No Name";
+                }
+                title = id + ", " + name + ", " + eMail;
+
+                if (title != null) {
+                    mTitleText.setText(title);
+                    mBookInput.setText("");
+                } else {
+                    // If none are found, update the UI to show failed results.
+                    mTitleText.setText("2");
+                }
+            } catch (Exception q) {
+                // If onPostExecute does not receive a proper JSON string, update the UI to show failed results.
+                mTitleText.setText("1");
+                e.printStackTrace();
+            }
         }
     }
 
